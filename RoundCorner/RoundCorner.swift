a00ad5a7db77f1f806a6037117748d1970111dd6
李东喜 <863223764@qq.com>
2018-08-20 18:02:37 +0800

    add namespace dx
    添加命名空间

---
+++ RoundCorner/RoundCorner.swift
@@ -0,0 +1,65 @@
+//
+//  RoundCorner.swift
+//  UIViewRoundCorner
+//
+//  Created by fashion on 2018/8/20.
+//  Copyright © 2018年 shangZhu. All rights reserved.
+//
+
+import UIKit
+
+public protocol RoundCornerCompatible {
+
+}
+
+public extension RoundCornerCompatible {
+    public var dx : RoundCorner<Self> {
+        get { return RoundCorner(self) }
+    }
+}
+
+public final class RoundCorner<Base> {
+    
+    public let base : Base
+    public init(_ base : Base){
+        self.base = base
+    }
+}
+
+
+extension UIView : RoundCornerCompatible {}
+
+extension RoundCorner where Base: UIView{
+    /// 设置一个四角圆角
+    ///
+    /// - Parameters:
+    ///   - radius: 圆角半径
+    ///   - cornerColor: 圆角背景色
+    public func roundCorner(radius: CGFloat,cornerColor: UIColor)  {
+        
+        base.layer.dx_roundCorner(radius: radius, cornerColor: cornerColor)
+    }
+    
+    /// 设置一个普通圆角
+    ///
+    /// - Parameters:
+    ///   - radius: 圆角半径
+    ///   - cornerColor: 圆角背景色
+    ///   - corners: 圆角位置
+    public func roundCorner(radius: CGFloat,cornerColor: UIColor,corners: UIRectCorner) {
+        base.layer.dx_roundCorner(radius: radius, cornerColor: cornerColor, corners: corners)
+    }
+    
+    ///  设置一个带边框的圆角
+    ///
+    /// - Parameters:
+    ///   - radius: 圆角半径
+    ///   - cornerColor: 圆角背景色
+    ///   - corners: 圆角位置
+    ///   - borderColor: 边框颜色
+    ///   - borderWidth: 边框线宽
+    func roundCorner(radii: CGSize,cornerColor: UIColor, corners: UIRectCorner, borderColor: UIColor, borderWidth: CGFloat)  {
+        base.layer.dx_roundCorner(cornerRadii: radii, cornerColor: cornerColor, corners: corners, borderColor: borderColor, borderWidth: borderWidth)
+    }
+}
+
--- UIViewRoundCorner/DXTestRoundedCell.swift
+++ UIViewRoundCorner/DXTestRoundedCell.swift
@@ -33,7 +33,7 @@ class DXTestRoundedCell: UITableViewCell {
             circle.frame = CGRect.init(x: 20*i, y: 0, width: 15, height: 15)
            // let color = i % 2 == 0 ? UIColor.white : UIColor.gray
             let color = UIColor.white
-            circle.dx_roundCorner(radius: 15/2, cornerColor: color)
+            circle.dx.roundCorner(radius: 15/2, cornerColor: color)
             circleView.addSubview(circle)
         }
         let label = UILabel()
@@ -47,14 +47,14 @@ class DXTestRoundedCell: UITableViewCell {
         
         // 注意:非正方形,四角圆形需要用[.topLeft,.topRight, .bottomRight, .bottomLeft]
         // 而不是用UIRectCorner.allCorners
-        label.dx_roundCorner(radius: 20/2, cornerColor: UIColor.white,corners: [.topLeft,.topRight, .bottomRight, .bottomLeft])
-        
+        label.dx.roundCorner(radius: 20/2, cornerColor: UIColor.white,corners: [.topLeft,.topRight, .bottomRight, .bottomLeft])
+
         // 必须要加layer.masksToBounds,否则显示不出来
         descLabel.layer.masksToBounds = true
       //  descLabel.layer.isOpaque = true
-        descLabel.dx_roundCorner(radius: 20/2, cornerColor: UIColor.white, corners: [.topLeft,.topRight])
+        descLabel.dx.roundCorner(radius: 20/2, cornerColor: UIColor.white, corners: [.topLeft,.topRight])
 
-        headView.dx_roundCorner(radii: CGSize.init(width: 35, height: 35), cornerColor: UIColor.white, corners: .allCorners, borderColor: UIColor.red, borderWidth: 2)
+        headView.dx.roundCorner(radii: CGSize.init(width: 35, height: 35), cornerColor: UIColor.white, corners: .allCorners, borderColor: UIColor.red, borderWidth: 2)
         
 //        let img = UIImage.dx_maskRoundCornerRadiusImage(color: UIColor.white, cornerRadii: CGSize.init(width: 35, height: 35), size: CGSize.init(width: 70, height: 70), corners: [.topLeft, .bottomRight], borderColor: nil, borderWidth: nil)
 //        topImageV.image = img
@@ -90,13 +90,13 @@ class DXTestRoundedCell: UITableViewCell {
         let color = selected ? UIColor.lightGray : UIColor.white
         self.contentView.backgroundColor = color
         
-        descLabel.dx_roundCorner(radius: 20/2, cornerColor: color, corners: [.topLeft,.topRight])
-        middleLabel.dx_roundCorner(radius: 20/2, cornerColor: color,corners: [.topLeft,.topRight, .bottomRight, .bottomLeft])
-        headView.dx_roundCorner(radii: CGSize.init(width: 35, height: 35), cornerColor: color, corners: .allCorners, borderColor: UIColor.red, borderWidth: 2)
+        descLabel.dx.roundCorner(radius: 20/2, cornerColor: color, corners: [.topLeft,.topRight])
+        middleLabel.dx.roundCorner(radius: 20/2, cornerColor: color,corners: [.topLeft,.topRight, .bottomRight, .bottomLeft])
+        headView.dx.roundCorner(radii: CGSize.init(width: 35, height: 35), cornerColor: color, corners: .allCorners, borderColor: UIColor.red, borderWidth: 2)
         
         circleView.backgroundColor = color
         for item in circleView.subviews {
-            item.dx_roundCorner(radius: 15/2, cornerColor: color)
+            item.dx.roundCorner(radius: 15/2, cornerColor: color)
         }
     }
     
